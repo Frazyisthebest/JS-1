27e217082659fc1baf4c095d670c59ff45d09762
khabibullo <khabibullo@gmail.com>
2021-08-19 11:29:36 +0500

    JS-1

---
+++ script.js
@@ -0,0 +1,34 @@
+var first = prompt('Ведите своё имя')
+
+console.log(first);
+
+var second = prompt('Сколько вам лет');
+
+console.log(second);
+
+var third = prompt('6 + 9 = ');
+
+console.log('6 + 9 = 15'+ ' (Ваш ответ '  + third);
+
+var fourth = prompt('9 - 6 = ');
+
+console.log('9 - 6 = 3'+ ' (Ваш ответ '  + fourth);
+
+var fiveth = prompt('9 * 6 = ');
+
+console.log('9 * 6 = 54'+ ' (Ваш ответ '  + fiveth);
+
+var sixth = prompt('9 : 6 =  ');
+
+console.log('9 : 6 = 1.5'+ ' (Ваш ответ '  + sixth);
+
+var seventh = prompt('9 % 6 = ');
+
+console.log('9 % 6 = 3'+ ' (Ваш ответ '  + seventh);
+
+var eighth = ('Зайдите в консоль')
+
+alert(eighth)
+
+
+
